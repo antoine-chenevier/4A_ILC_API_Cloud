0fbc1c88216383bd01e67faff6aaa55a4e869161
Antoine-CHENEVIER <antoine.chenevier01@gmail.com>
2023-03-10 10:04:08 +0100

    changes on app

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from datetime import datetime
-import pandas as pd
 import redis
+import json
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 r1 = redis.Redis(host='localhost', port=6379, db=1)
@@ -18,65 +18,59 @@ listOperation = []
 def tweet():
     global id_operation
     global listOperation
-      #  get the data from the request
+
+    #  get the data from the request
     user = str(request.form.get("user"))
     message=str(request.form.get("message"))
+
     #  save the tweet in list
     id_operation += 1
     date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    operation = {
+    
+    #Create a json object
+    data = {
         "id": id_operation,
         "user": user,
         "message": message,
         "date": date
     }
-    listOperation.append(operation)
-    r.set(user,operation)
-    r1.set(date,operation)
-    return operation
+    
+    #Json to string
+    data = json.dumps(data)
+    
+    #Save in redis
+    r.set(user,data)
+    r1.set(date,data)
+
+    return data
 
 
 # get all the tweets
 @app.route('/tweets', methods=['GET'])
 def tweets():
-    return {"tweets": listOperation}
+    list = []
+
+    for i in r.scan_iter():
+        message = r.get(i)
+        message= str(message)
+        list.append(message)
+    return str(list)
 
 # get the tweets of a user
 @app.route('/tweets/<user>', methods=['GET'])
 def tweetsUser(user):
+    list = []
+    # Get the data from a user
+    for i in r.scan_iter():
+       
+        message = r.get(i)
+        message = str(message)
+        #  filter the tweets of the user
+        value = "user:" + user
+        if(value in message):
+            list.append(message)
     #  filter the tweets of the user
-    tweets = list(filter(lambda tweet: tweet["user"] == user, listOperation))
-    return {"tweets": tweets}
-
-# save a tweet in redis
-@app.route('/saveTweet', methods=['POST'])
-def saveTweet():
-    #  get the data from the request
-    user = str(request.form.get("user"))
-    message=str(request.form.get("message"))
-    #  save the tweet in redis
-    r.set(user,message)
-    return {"message": "tweet saved"}
-
-# attribute a tweet to a user
-@app.route('/attributeTweet', methods=['POST'])
-def attributeTweet():
-    #  get the data from the request
-    user = str(request.form.get("user"))
-    #  get the tweet from redis
-    tweet = r.get(user)
-    #  create a new operation
-    id_operation += 1
-    operation = {
-        "id": id_operation,
-        "user": user,
-        "message": tweet.decode("utf-8"),
-        "date": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    }
-    #  add the operation to the list
-    listOperation.append(operation)
-    #  return the operation
-    return operation
+    return str(list)
 
 # retweet a tweet
 @app.route('/retweet', methods=['POST'])
@@ -101,8 +95,17 @@ def retweet():
 # get the tweets for a hashtag
 @app.route('/tweetsHashtag/<hashtag>', methods=['GET'])
 def tweetsHashtag(hashtag):
-    #  filter the tweets of the user
-    tweets = list(filter(lambda tweet: hashtag in tweet["message"], listOperation))
-    return {"tweets": tweets}
+    list = []
+    hashtag = "#" + hashtag
 
-# get the hastag 
+    # Get the data from a user
+    for i in r.scan_iter():
+       
+        message = r.get(i)
+        message = str(message)
+        # get the hastag 
+        if(hashtag in message):
+            list.append(message)
+    return str(list)
+    
+
