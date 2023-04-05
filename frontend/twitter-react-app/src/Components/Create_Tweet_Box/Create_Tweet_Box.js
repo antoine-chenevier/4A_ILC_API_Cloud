0a7f69ecb59f4207be065e56ac470abe909d5938
Christian <Christian_Hasbani@etu.u-bourgogne.fr>
2023-04-05 19:54:36 +0200

    post on main page is done

--- frontend/twitter-react-app/src/Components/Create_Tweet_Box/Create_Tweet_Box.js
+++ frontend/twitter-react-app/src/Components/Create_Tweet_Box/Create_Tweet_Box.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Create_Tweet_Box.css";
 
-const Create_Tweet_Box = () => {
-  return <div className="create-tweet-box">
-    <span className="create-tweet-box-header">
-    <h6 className="create-tweet-box-header-text"> POST A NEW TWEET</h6>
-    </span>
-    <form>
-        <input className="create-tweet-box-user" type="text" placeholder="User" />
-        <textarea className="create-tweet-box-message" placeholder="Message"></textarea>
-        <span className="create-tweet-box-postBtn"><button className="create-tweet-box-postBtn-btn"  type="submit">Tweet</button></span>
-    </form>
-  </div>;
-};
+const Create_Tweet_Box = ({ onTweetPosted }) => {
+  const [user, setUser] = useState("");
+  const [message, setMessage] = useState("");
 
-export default Create_Tweet_Box;
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `user=${user}&message=${message}`,
+    };
+    const response = await fetch("http://localhost:5000/tweet", requestOptions);
+    const data = await response.json();
+    console.log(data); // Print the response from the server
+    setUser("");
+    setMessage("");
+    onTweetPosted(); // Call the onTweetPosted function to notify Home component
+  };
+
+  return (
+    <div className="create-tweet-box">
+      <span className="create-tweet-box-header">
+        <h6 className="create-tweet-box-header-text"> POST A NEW TWEET</h6>
+      </span>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          name="user"
+          className="create-tweet-box-user"
+          type="text"
+          placeholder="User"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <textarea
+          name="message"
+          className="create-tweet-box-message"
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <span className="create-tweet-box-postBtn">
+          <button className="create-tweet-box-postBtn-btn" type="submit">
+            Tweet
+          </button>
+        </span>
+      </form>
+    </div>
+);   
+  };
+  export default Create_Tweet_Box;
--- frontend/twitter-react-app/src/Routes/Home.js
+++ frontend/twitter-react-app/src/Routes/Home.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Create_Tweet_Box from "../Components/Create_Tweet_Box/Create_Tweet_Box";
 import Tweet_Box from "../Components/Tweet_Box/Tweet_Box";
 import useFetchAllTweets from "../Hooks/useFetchAllTweets";
 import "./Home.css";
 
 function Home() {
+  const [tweetPosted, setTweetPosted] = useState(false); 
   const tweets = useFetchAllTweets();
+
+  useEffect(() => {
+    if (tweetPosted) {
+      // Fetch the latest tweets again after a new tweet is posted
+      setTweetPosted(false);
+    }
+  }, [tweetPosted]);
+
   return (
     <div className="Home">
       <h1>HOME</h1>
-      <Create_Tweet_Box/>
+      <Create_Tweet_Box tweetPosted={tweetPosted} setTweetPosted={setTweetPosted} />
       {tweets ? (
         tweets.map((tweet) => (
           <Tweet_Box key={tweet.id} id={tweet.id} user={tweet.user} message={tweet.message} date={tweet.date}/>
@@ -22,4 +31,3 @@ function Home() {
 }
 
 export default Home;
-
